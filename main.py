5750d530c3f83832d266e4436bbce9084dbf7b82
GusVieweg <awvieweg@gmail.com>
2020-01-06 09:20:20 -0500

    started scheduler

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ jb.run(os.environ["ACCESS_TOKEN"])
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', days=1, start_date='2020-01-02 23:59:00', timezone="US/Eastern")
+@sched.scheduled_job('interval', days=1, start_date='2020-01-06 23:59:00', timezone="US/Eastern")
 async def scheduled_job():
+    print("Running scheduled_job()")
     await jb.nightly_cloud_reset()
+
+sched.start()
