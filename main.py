c8ab88bdd79c6d5515aa978fe7b9b10f959cd29b
Gus Vieweg <awvieweg@gmail.com>
2020-01-07 11:56:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ load_dotenv()
 
 jb = JokeyBot()
 
-sched = BlockingScheduler()
+# sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', days=1, start_date='2020-01-06 23:59:00', timezone="US/Eastern")
-async def scheduled_job():
-    print("Running scheduled_job()")
-    await jb.nightly_cloud_reset()
+# @sched.scheduled_job('interval', days=1, start_date='2020-01-06 23:59:00', timezone="US/Eastern")
+# async def scheduled_job():
+#     print("Running scheduled_job()")
+#     await jb.nightly_cloud_reset()
 
-sched.start()
+# sched.start()
 
 jb.run(os.environ["ACCESS_TOKEN"])
